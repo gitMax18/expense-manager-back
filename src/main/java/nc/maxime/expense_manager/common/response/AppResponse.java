f17bd101a25e0f86b7869b3dd75360ed5f40e63d
JEAN Maxime <jean.maxime0988@gmail.com>
2025-10-20 16:41:50 +0700

    refac(appResponse): use builder pattern

--- src/main/java/nc/maxime/expense_manager/common/response/AppResponse.java
+++ src/main/java/nc/maxime/expense_manager/common/response/AppResponse.java
@@ -2,7 +2,27 @@ package nc.maxime.expense_manager.common.response;
 
 public record AppResponse<TData>(boolean success, String message, TData data) {
 
+    public AppResponse {
+        success = true;
+    }
+
     public AppResponse(String message, TData data) {
         this(true, message, data);
+    }
+
+    public static Builder message(String message) {
+        return new Builder(message);
+    }
+
+    public static final class Builder {
+        private final String message;
+
+        private Builder(String message) {
+            this.message = message;
+        }
+
+        public <TData> AppResponse<TData> data(TData data) {
+            return new AppResponse<>(true, message, data);
+        }
     }
 }
