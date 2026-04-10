aafcdfe710d5a4f8c9842b1eacad77ca510c0d4c
JEAN Maxime <jean.maxime0988@gmail.com>
2026-04-10 16:28:07 +0700

    feat(categoryExpense): add color to repository

--- src/main/java/nc/maxime/expense_manager/dashboard/DashboardRepository.java
+++ src/main/java/nc/maxime/expense_manager/dashboard/DashboardRepository.java
@@ -11,17 +11,18 @@ import org.springframework.data.repository.query.Param;
 
 public interface DashboardRepository extends JpaRepository<Transaction, Long> {
 
-    @Query("""
-            SELECT new nc.maxime.expense_manager.dashboard.CategoryExpense(
-                c.id,
-                c.name,
-                SUM(t.amount))
-            FROM Transaction t
-            LEFT JOIN t.category c
-            WHERE t.type IN :types
-              AND t.account.user = :user
-            GROUP BY c.id, c.name
-            """)
-    List<CategoryExpense> findTotalsByTypeAndCategory(
-            @Param("user") User user, @Param("types") Collection<TransactionType> types);
+  @Query("""
+      SELECT new nc.maxime.expense_manager.dashboard.CategoryExpense(
+          c.id,
+          c.name,
+          SUM(t.amount),
+          c.color)
+      FROM Transaction t
+      LEFT JOIN t.category c
+      WHERE t.type IN :types
+        AND t.account.user = :user
+      GROUP BY c.id, c.name, c.color
+      """)
+  List<CategoryExpense> findTotalsByTypeAndCategory(
+      @Param("user") User user, @Param("types") Collection<TransactionType> types);
 }
